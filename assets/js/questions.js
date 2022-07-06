70d246dee94e5f4ead7132181106dfcbc7b0eeb7
Brad <bycarlson23@gmail.com>
2022-07-06 14:03:23 -0500

    initial commit

---
+++ assets/js/questions.js
@@ -0,0 +1,17 @@
+var questions = [
+    {
+        questionText: "How to declare a variable in JavaScript?",
+        choices: ["var", "for", "while", "if"],
+        answer: "var"
+    },
+    {
+        questionText: "?",
+        choices: ["", "", "", ""],
+        answer: ""
+    },
+    {
+        questionText: "",
+        choices: ["", "", "", ""],
+        answer: ""
+    }
+]
